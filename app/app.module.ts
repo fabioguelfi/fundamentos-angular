6fbc181b6eabac74060171f7cce15f1f3d120858
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-15 18:13:22 -0300

    curso-angular-06-primeiro-modulo

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
-    AppComponent
-  ]
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}

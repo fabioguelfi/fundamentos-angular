39aa4ff4d06d4d0814e607f3d1a68270ae1c1c4f
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-15 18:03:44 -0300

    curso-angular-05-primeiro-componente

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Angular Fundamentals
-    </div>
-  `
+    selector: 'app-root',
+    styleUrls: ['app.component.scss'],
+    template: `
+      <div class="app">
+        {{ title }}
+      </div>
+    `
 })
 export class AppComponent {
+
+  public title: string;
+
+  constructor() {
+    this.title = `Angular Fundamentos`
+  }
 
 }

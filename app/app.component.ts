62873b2b187f1573503d985c94b227e2357f9ad2
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-15 20:30:15 -0300

    curso-angular-15-ng-for

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,26 +5,33 @@ import { Component } from "@angular/core";
   styleUrls: ["app.component.scss"],
   template: `
       <div class="app">
-        <input
-        type="text"
-        (input)="handleInput($event.target.value)"
-        >
-      <div *ngIf="name.length > 5; else template">
-        Buscando por ... {{ name }}
-      </div>
-      <ng-template #template>
-        <div>
-        Aguarde, estamos consultando nossa base de dados ...
-        </div>
-      </ng-template>
+        <li *ngFor="let passageiro of passageiros; index as i">
+          {{ i + 1 }}: {{ passageiro.fullname }}
+        </li>
       </div>
     `
 })
 export class AppComponent {
-  public name: string = "";
-  constructor() {}
-
-  private handleInput(value: string): void {
-    this.name = value;
-  }
+  public passageiros: any = [
+    {
+      id: 1,
+      fullname: 'Fabio Guelfi',
+      checkedIn: false,
+    },
+    {
+      id: 2,
+      fullname: 'Renata Guelfi',
+      checkedIn: true,
+    },
+    {
+      id: 3,
+      fullname: 'Rodrigo Guelfi',
+      checkedIn: false,
+    },
+    {
+      id: 4,
+      fullname: 'Ricardo Guelfi',
+      checkedIn: true,
+    }
+  ];
 }
